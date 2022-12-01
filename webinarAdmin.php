008e0d51db6af7b63648f9195d0555fd91c6630a
SetyoarieA <setyoarieanggara@gmail.com>
2022-12-01 10:09:02 +0700

    modifikasi html dan css fix

--- webinarAdmin.php
+++ webinarAdmin.php
@@ -24,7 +24,7 @@
         </div>
     </nav>
 
-    <!-- Content -->
+    <!-- Detail Webinar -->
     <section id="kelolawebinar">
         <div class="inner-widht">
             <div class="buttons">
